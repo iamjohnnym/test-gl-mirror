480af05c8362db02dbddd673759590b2a08e3325
John Martin <jmartin@belong.gg>
2022-10-19 14:17:16 -0500

    fix: add a terraform config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const dockerConfig = require('./docker');
 const npmConfig = require('./npm');
+const terraformConfig = require('./terraform');
 module.exports = {
   dockerConfig,
-  npmConfig
+  npmConfig,
+  terraformConfig
 }
