32e3401237b5ba9608c14013a6d8051995073243
John Martin <jmartin@belong.gg>
2023-01-08 22:47:12 -0600

    fix: update docker projectPath

--- src/plugins/docker.js
+++ src/plugins/docker.js
@@ -7,11 +7,11 @@ function configureDocker(o) {
   }
   const docker = o.plugins.docker
   if (o.dockerMulti) {
-    if (!options.projectPath) {
+    if (!o.projectPath) {
       throw 'Invalid Docker Multi config. Must set projectPath param in any \
             docker project\'s release config.\nSee README."'
     }
-    const packageJson = openJsonFile(`${options.projectPath}/package.json`)
+    const packageJson = openJsonFile(`${o.projectPath}/package.json`)
     docker.dockerTags = [
         `{{#if prerelease.[0]}}${packageJson.docker_version}-{{prerelease.[0]}}{{else}}${packageJson.docker_version}-latest{{/if}}`,
         `${packageJson.docker_version}-{{version}}`
