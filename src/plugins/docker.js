dfa082f023ac906973b55be75eea1c60efaf03bc
John Martin <johnny@iamjohnnym.com>
2023-01-09 00:29:27 -0600

    fix: leverage @beepbeepgo/semantic-release-multi
    forked multi-semantic-release and modified it so that it reveals the
    subprojects as an envvar, making it ready for consumption.

--- src/plugins/docker.js
+++ src/plugins/docker.js
@@ -7,14 +7,10 @@ function configureDocker(o) {
   }
   const docker = o.plugins.docker
   if (o.dockerMulti) {
-    if (!o.projectPath) {
-      throw 'Invalid Docker Multi config. Must set projectPath param in any \
-            docker project\'s release config.\nSee README."'
-    }
-    const packageJson = openJsonFile(`${o.projectPath}/package.json`)
+    const packageJson = openJsonFile(`${process.env.npm_workspace_path}/package.json`)
     docker.dockerTags = [
-        `{{#if prerelease.[0]}}${packageJson.docker_version}-{{prerelease.[0]}}{{else}}${packageJson.docker_version}-latest{{/if}}`,
-        `${packageJson.docker_version}-{{version}}`
+        `{{#if prerelease.[0]}}${packageJson.name}-{{prerelease.[0]}}{{else}}${packageJson.name}-latest{{/if}}`,
+        `${packageJson.name}-{{version}}`
       ]
   }
   return [
