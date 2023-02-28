99f2af16e2e9c1201641521fc44f2dc989855c4b
John Martin <johnny@iamjohnnym.com>
2023-02-28 02:13:53 -0600

    fix: allow gitlab args

--- src/plugins/gitlab.js
+++ src/plugins/gitlab.js
@@ -1,10 +1,13 @@
+const { merge } = require("../utils");
+
 function configureGitlab(o) {
   if (!o.gitlab) {
-    return []
+    return [];
   }
-  return ['@semantic-release/gitlab']
+  const gitlab = o.plugins.gitlab;
+  return [["@semantic-release/gitlab", merge({}, gitlab)]];
 }
 
 module.exports = {
-  configureGitlab
-}
+  configureGitlab,
+};
